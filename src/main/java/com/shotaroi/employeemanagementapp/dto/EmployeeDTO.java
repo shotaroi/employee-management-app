69bdf88814c97660cc714fb0608babb948923c86
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-20 14:05:34 +0100

    Update EmployeeDTO.java

--- src/main/java/com/shotaroi/employeemanagementapp/dto/EmployeeDTO.java
+++ src/main/java/com/shotaroi/employeemanagementapp/dto/EmployeeDTO.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class EmployeeDto {
+public class EmployeeDTO {
     private Long id;
     private String firstName;
     private String lastName;
