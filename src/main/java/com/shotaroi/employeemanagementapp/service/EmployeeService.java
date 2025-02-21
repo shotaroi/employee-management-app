2fb450ca59a3e4f3545f111a2ef3e7a7644cce05
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-21 01:39:32 +0100

    Update EmployeeService.java

--- src/main/java/com/shotaroi/employeemanagementapp/service/EmployeeService.java
+++ src/main/java/com/shotaroi/employeemanagementapp/service/EmployeeService.java
@@ -1,4 +1,17 @@
 package com.shotaroi.employeemanagementapp.service;
 
+import com.shotaroi.employeemanagementapp.dto.EmployeeDTO;
+
+import java.util.List;
+
 public interface EmployeeService {
+    EmployeeDTO createEmployee(EmployeeDTO employeeDTO);
+
+    EmployeeDTO getEmployeeById(Long id);
+
+    List<EmployeeDTO> getAllEmployees();
+
+    EmployeeDTO updateEmployee(Long employeeId, EmployeeDTO updateEmployee);
+
+    void deleteEmployee(Long employeeId);
 }
