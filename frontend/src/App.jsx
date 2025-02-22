977ea55744d6a7f4229194ed2ecca27150245fc7
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-22 11:19:56 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
 
 function App() {
 
   return (
     <>
+      <HeaderComponent />
       <ListEmployeeComponent />
+      <FooterComponent />
     </>
   )
 }
