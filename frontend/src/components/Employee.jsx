3b58aca902adac090df0889ff4b1179b7e378b33
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-23 23:52:23 +0100

    Update Employee.jsx

--- frontend/src/components/Employee.jsx
+++ frontend/src/components/Employee.jsx
@@ -1,74 +1,87 @@
-import { useState } from "react";
-import { createEmployee } from "../services/EmployeeService";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  createEmployee,
+  getEmployee,
+  updateEmployee,
+} from "../services/EmployeeService";
+import { useNavigate, useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 const Employee = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
+
+  const { id } = useParams();
   const navigator = useNavigate();
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ mode: "onSubmit" });
+  } = useForm();
 
-  const saveEmployee = (e) => {
-    e.preventDefault();
-
-    const newEmployee = { firstName, lastName, email };
-
-    createEmployee(newEmployee)
-      .then(() => {
-        navigator("/employees");
-      })
-      .catch((error) => {
-        console.log("hmm?");
-        console.error(
-          "Axios Error:",
-          error.response ? error.response.data : error.message
-        );
-      });
+  const onSubmit = (employee) => {
+    if (id) {
+      updateEmployee(id, employee)
+        .then(() => {
+          navigator("/employees");
+        })
+        .catch((error) => console.error(error));
+    } else {
+      createEmployee(employee)
+        .then(() => {
+          navigator("/employees");
+        })
+        .catch((error) => {
+          console.log("hmm?");
+          console.error(
+            "Axios Error:",
+            error.response ? error.response.data : error.message
+          );
+        });
+    }
   };
 
-  const onSubmit = () => {
-    const newEmployee = { firstName, lastName, email };
-
-    createEmployee(newEmployee)
-      .then(() => {
-        navigator("/employees");
-      })
-      .catch((error) => {
-        console.log("hmm?");
-        console.error(
-          "Axios Error:",
-          error.response ? error.response.data : error.message
-        );
-      });
+  const pageTitle = () => {
+    if (id) {
+      return <h2 className="text-center m-4">Update Employee</h2>;
+    } else {
+      return <h2 className="text-center m-4">Add Employee</h2>;
+    }
   };
+
+  useEffect(() => {
+    if (id) {
+      getEmployee(id)
+        .then((response) => {
+          setFirstName(response.data.firstName);
+          setLastName(response.data.lastName);
+          setEmail(response.data.email);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [id]);
 
   return (
     <div className="container">
       <div className="row">
         <div className="card col-md-6 offset-md-3">
-          <h2 className="text-center m-4">Add Employee</h2>
+          {pageTitle()}
           <div className="card-body">
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="form-group m-3">
-                <label htmlFor="" className="form-label">
-                  First Name
-                </label>
+                <label className="form-label">First Name</label>
                 <input
                   type="text"
                   className="form-control"
                   placeholder="Enter First Name"
+                  value={firstName}
                   name="firstName"
-                  //   value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
                   {...register("firstName", {
-                    required: "First Name is required",
+                    required: "First Name required",
                   })}
                 />
                 {errors.firstName && (
@@ -76,18 +89,15 @@ const Employee = () => {
                 )}
               </div>
               <div className="form-group m-3">
-                <label htmlFor="" className="form-label">
-                  Last Name
-                </label>
+                <label className="form-label">Last Name</label>
                 <input
                   type="text"
                   className="form-control"
                   placeholder="Enter Last Name"
+                  value={lastName}
                   name="lastName"
-                  //   value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
                   {...register("lastName", {
-                    required: "Last Name is required",
+                    required: "Last Name required",
                   })}
                 />
                 {errors.lastName && (
@@ -95,32 +105,25 @@ const Employee = () => {
                 )}
               </div>
               <div className="form-group m-3">
-                <label htmlFor="" className="form-label">
-                  Email
-                </label>
+                <label className="form-label">Email</label>
                 <input
                   type="email"
                   className="form-control"
                   placeholder="Enter Email"
+                  value={email}
                   name="email"
-                  //   value={email}
-                  onChange={(e) => setEmail(e.target.value)}
                   {...register("email", {
-                    required: "Email is required",
+                    required: "Email required",
                   })}
                 />
                 {errors.email && (
                   <p style={{ color: "red" }}>{errors.email.message}</p>
                 )}
               </div>
+              <button className="btn btn-success m-3" type="submit">
+                Submit
+              </button>
             </form>
-            <button
-              className="btn btn-success m-3"
-              type="submit"
-              //   onClick={saveEmployee}
-            >
-              Submit
-            </button>
           </div>
         </div>
       </div>
