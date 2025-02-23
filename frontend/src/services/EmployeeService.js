f143d71c13f1e38e096c31e1c3e4b5479c271fa3
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-23 23:52:28 +0100

    Update EmployeeService.js

--- frontend/src/services/EmployeeService.js
+++ frontend/src/services/EmployeeService.js
@@ -5,3 +5,7 @@ const API_URL = "http://localhost:8080/api/employees";
 export const listEmployees = () => axios.get(API_URL);
 
 export const createEmployee = (emp) => axios.post(API_URL, emp);
+
+export const getEmployee = (empId) => axios.get(API_URL + '/' + empId);
+
+export const updateEmployee = (empId, emp) => axios.put(API_URL + '/' + empId, emp);
