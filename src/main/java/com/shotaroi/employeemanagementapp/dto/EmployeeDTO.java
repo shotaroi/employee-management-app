7270c1bbcb6e33fb0608fd0085eef83492e97838
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-25 23:19:10 +0100

    Update EmployeeDTO.java

--- src/main/java/com/shotaroi/employeemanagementapp/dto/EmployeeDTO.java
+++ src/main/java/com/shotaroi/employeemanagementapp/dto/EmployeeDTO.java
@@ -1,17 +1,22 @@
 package com.shotaroi.employeemanagementapp.dto;
 
+import com.shotaroi.employeemanagementapp.entity.Department;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Date;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeDTO {
-    private Long id;
+    private Long employeeId;
     private String firstName;
     private String lastName;
     private String email;
+    private Date joiningDate;
+    private Long departmentId;
 }
