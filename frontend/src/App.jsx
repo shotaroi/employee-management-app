5be3a4c16c3c1a47fd75a4106688470f6c6b20ba
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-25 23:19:45 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import EmployeeList from "./components/EmployeeList";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Employee from "./components/Employee";
+import ListDepartments from "./components/DepartmentList";
+import Department from "./components/Department";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -15,6 +17,9 @@ function App() {
           <Route path="/employees" element={<EmployeeList />}></Route>
           <Route path="/add-employee" element={<Employee />}></Route>
           <Route path="edit-employee/:id" element={<Employee />}></Route>
+          <Route path="/departments" element={<ListDepartments />}></Route>
+          <Route path="/add-department" element={<Department />}></Route>
+          <Route path="/edit-department/:id" element={<Department />}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
