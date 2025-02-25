0ee30ff27c7bf84f67adbbf157444c28a2e661fb
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-25 23:19:35 +0100

    Update Employee.jsx

--- frontend/src/components/Employee.jsx
+++ frontend/src/components/Employee.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import {
   createEmployee,
   getEmployee,
@@ -6,11 +6,12 @@ import {
 } from "../services/EmployeeService";
 import { useNavigate, useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { getAllDepartments } from "../services/DepartmentService";
 
 const Employee = () => {
-
   const { id } = useParams();
   const navigator = useNavigate();
+  const [departments, setDepartments] = useState([]);
 
   const {
     register,
@@ -20,9 +21,10 @@ const Employee = () => {
     formState: { errors },
   } = useForm({
     defaultValues: {
-        firstName: "",
-        lastName: "",
-        email: "",
+      department: "",
+      firstName: "",
+      lastName: "",
+      email: "",
     },
   });
 
@@ -50,18 +52,22 @@ const Employee = () => {
 
   const pageTitle = () => {
     if (id) {
-      return <h2 className="text-center m-4">Update Employee</h2>;
+      return <h2 className="text-center m-4">Edit Employee</h2>;
     } else {
       return <h2 className="text-center m-4">Add Employee</h2>;
     }
   };
 
   useEffect(() => {
+    getAllDepartments()
+      .then((response) => setDepartments(response.data))
+      .catch((error) => console.error(error));
+
     if (id) {
       getEmployee(id)
         .then((response) => {
-            const {firstName, lastName, email} = response.data;
-            reset({ firstName, lastName, email });
+          const { department, firstName, lastName, email } = response.data;
+          reset({ department, firstName, lastName, email });
         })
         .catch((error) => {
           console.error(error);
@@ -77,6 +83,20 @@ const Employee = () => {
           <div className="card-body">
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="form-group m-3">
+                <label className="form-label">Department</label>
+                <select className="form-control" {...register("department", {required: "Department is required"})}>
+                  <option value="" disabled selected>Select Department</option>
+                  {departments.map((dept) => (
+                    <option key={dept.departmentId} value={dept}>
+                      {dept.name}
+                    </option>
+                  ))}
+                </select>
+                {errors.department && (
+                  <p style={{ color: "red" }}>{errors.department.message}</p>
+                )}
+              </div>
+              <div className="form-group m-3">
                 <label className="form-label">First Name</label>
                 <input
                   type="text"
@@ -84,7 +104,7 @@ const Employee = () => {
                   placeholder="Enter First Name"
                   name="firstName"
                   {...register("firstName", {
-                    required: "First Name required",
+                    required: "First Name is required",
                   })}
                 />
                 {errors.firstName && (
@@ -99,7 +119,7 @@ const Employee = () => {
                   placeholder="Enter Last Name"
                   name="lastName"
                   {...register("lastName", {
-                    required: "Last Name required",
+                    required: "Last Name is required",
                   })}
                 />
                 {errors.lastName && (
@@ -114,11 +134,26 @@ const Employee = () => {
                   placeholder="Enter Email"
                   name="email"
                   {...register("email", {
-                    required: "Email required",
+                    required: "Email is required",
                   })}
                 />
                 {errors.email && (
                   <p style={{ color: "red" }}>{errors.email.message}</p>
+                )}
+              </div>
+              <div className="form-group m-3">
+                <label className="form-label">Joning Date</label>
+                <input
+                  type="date"
+                  className="form-control"
+                  placeholder="Choose Date"
+                  name="joiningDate"
+                  {...register("joiningDate", {
+                    required: "Date is required",
+                  })}
+                />
+                {errors.joiningDate && (
+                  <p style={{ color: "red" }}>{errors.joiningDate.message}</p>
                 )}
               </div>
               <button className="btn btn-success m-3" type="submit">
