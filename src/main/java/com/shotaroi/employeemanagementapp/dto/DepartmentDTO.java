3b2535a761ad9e5726709445b075bcfe968ca923
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-25 10:38:32 +0100

    Update DepartmentDTO.java

--- src/main/java/com/shotaroi/employeemanagementapp/dto/DepartmentDTO.java
+++ src/main/java/com/shotaroi/employeemanagementapp/dto/DepartmentDTO.java
@@ -1,4 +1,16 @@
 package com.shotaroi.employeemanagementapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DepartmentDTO {
+    private Long Id;
+    private String departmentName;
+    private String departmentDescription;
 }
