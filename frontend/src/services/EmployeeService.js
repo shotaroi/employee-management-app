e46c015f9da6c53a288a7c50d087f906a203e5e9
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-25 23:19:19 +0100

    Update EmployeeService.js

--- frontend/src/services/EmployeeService.js
+++ frontend/src/services/EmployeeService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/employees";
+const BASE_API_URL = "http://localhost:8080/api/employees";
 
-export const listEmployees = () => axios.get(API_URL);
+export const listEmployees = () => axios.get(BASE_API_URL);
 
-export const createEmployee = (emp) => axios.post(API_URL, emp);
+export const createEmployee = (emp) => axios.post(BASE_API_URL, emp);
 
-export const getEmployee = (empId) => axios.get(API_URL + '/' + empId);
+export const getEmployee = (empId) => axios.get(BASE_API_URL + '/' + empId);
 
-export const updateEmployee = (empId, emp) => axios.put(API_URL + '/' + empId, emp);
+export const updateEmployee = (empId, emp) => axios.put(BASE_API_URL + '/' + empId, emp);
 
-export const deleteEmployee = (empId) => axios.delete(API_URL + '/' + empId);
+export const deleteEmployee = (empId) => axios.delete(BASE_API_URL + '/' + empId);
