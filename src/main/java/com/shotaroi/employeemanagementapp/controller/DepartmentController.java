444bc9350ba5463c2326e7328aecfab7781a0293
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-25 10:38:38 +0100

    Create DepartmentController.java

---
+++ src/main/java/com/shotaroi/employeemanagementapp/controller/DepartmentController.java
@@ -0,0 +1,43 @@
+package com.shotaroi.employeemanagementapp.controller;
+
+import com.shotaroi.employeemanagementapp.dto.DepartmentDTO;
+import com.shotaroi.employeemanagementapp.service.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/departments")
+public class DepartmentController {
+
+    private DepartmentService departmentService;
+
+    @PostMapping
+    public ResponseEntity<DepartmentDTO> createDepartment(@RequestBody DepartmentDTO departmentDTO) {
+        DepartmentDTO department = departmentService.createDepartment(departmentDTO);
+        return new ResponseEntity<>(department, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<DepartmentDTO> getDepartmentById(@PathVariable("id") Long departmentId) {
+        DepartmentDTO departmentDTO = departmentService.getDepartmentById(departmentId);
+        return ResponseEntity.ok(departmentDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DepartmentDTO>> getAllDepartments() {
+        List<DepartmentDTO> departments = departmentService.getAllDepartments();
+        return ResponseEntity.ok(departments);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<DepartmentDTO> updateDepartment(@PathVariable("id") Long departmentId,
+                                                          @RequestBody DepartmentDTO updatedDepartmentDTO) {
+        DepartmentDTO departmentDTO = departmentService.updateDepartment(departmentId, updatedDepartmentDTO);
+        return ResponseEntity.ok(departmentDTO);
+    }
+}
