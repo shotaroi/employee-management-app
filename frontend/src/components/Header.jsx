5c06ab21404e4f182a365c55042c7a96f4af3f93
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-25 23:19:25 +0100

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-        <header>
-            <nav className='navbar navbar-dark bg-primary'>
-                <a href="" className="navbar-brand">Employee Management App</a>
-            </nav>
-        </header>
-    </div>
-  )
-}
+      <header>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+          <NavLink className="navbar-brand title" to="/">Employee Management App</NavLink>
 
-export default Header
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className='nav-link' to='/employees'>Employees</NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className='nav-link' to='/departments'>Departments</NavLink>
+              </li>
+              
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
