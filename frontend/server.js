cbe322c2f5de8c07ebd2cb050313f80162401b5d
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-03-03 02:43:30 +0100

    Create server.js

---
+++ frontend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// Serve static files from the React build directory
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Start the server
+const PORT = import.meta.env.VITE_PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
