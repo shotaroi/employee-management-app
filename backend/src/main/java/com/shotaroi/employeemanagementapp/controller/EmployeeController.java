bb4301ec14846193b04239e6cd13f1cd3ecefaf3
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-03-04 00:49:15 +0100

    Create EmployeeController.java

---
+++ backend/src/main/java/com/shotaroi/employeemanagementapp/controller/EmployeeController.java
@@ -0,0 +1,57 @@
+package com.shotaroi.employeemanagementapp.controller;
+
+import com.shotaroi.employeemanagementapp.dto.EmployeeDTO;
+import com.shotaroi.employeemanagementapp.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable("id") Long id) {
+            EmployeeDTO employeeDTO = employeeService.getEmployeeById(id);
+            return ResponseEntity.ok(employeeDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeDTO>> getAllEmployees() {
+        List<EmployeeDTO> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody @Validated EmployeeDTO employeeDTO) {
+        EmployeeDTO newEmployee = employeeService.createEmployee(employeeDTO);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDTO> updateEmployee(@PathVariable("id") Long id, @RequestBody EmployeeDTO employeeDTO) {
+        EmployeeDTO updatedEmployee = employeeService.updateEmployee(id, employeeDTO);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long id) {
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.ok("Employee with id " + id + " is successfully deleted");
+    }
+
+
+
+
+
+
+
+}
