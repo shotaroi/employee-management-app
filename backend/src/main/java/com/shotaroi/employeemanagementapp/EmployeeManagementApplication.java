ba673a8c1c864148f46e208d1c1ebf20dd3199d8
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-03-04 00:49:21 +0100

    Create EmployeeManagementApplication.java

---
+++ backend/src/main/java/com/shotaroi/employeemanagementapp/EmployeeManagementApplication.java
@@ -0,0 +1,14 @@
+package com.shotaroi.employeemanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementApplication.class, args);
+        System.out.println("hello world");
+    }
+
+}
